8dc822ca0c28cbc714a5b66a348f1ca07daf67d3
romanovich3313 <roman.romanovich.pl@gmail.com>
2023-03-08 17:04:06 +0300

    up

--- testList.py
+++ testList.py
@@ -1,8 +1,11 @@
 my_list = list()
 
-my_list1 = [1,2,3,4,5]
+my_list1 = [100,95,71,14,25]
 my_list2 = ["a","b","c","d","e"]
+my_list1.sort()
+#a = my_list1[0:3]
 
-my_list = [my_list1, my_list2]
+my_list = my_list1[1:3] + my_list2
 
 print(my_list)
+
